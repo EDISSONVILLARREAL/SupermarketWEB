a815036677dad416b5c4e4edeffa0375d7fe578e
EDISSON VILLARREAL <cuentacecep@gmail.com>
2025-05-08 02:07:56 -0500

    Main View

---
+++ Data/SumpermarketContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SupermarketWEB.Data
+{
+    public class SupermarketContext : DbContext
+    {
+        public SupermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected SupermarketContext()
+        {
+        }
+    }
+}
